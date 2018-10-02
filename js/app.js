f3ac79a21cdbdccc1022dc2ace5d0eec060b3c0d
Paul Tiller <pftiller@gmail.com>
2018-10-02 12:24:16 -0500

    cards back and front

--- js/app.js
+++ js/app.js
@@ -16,23 +16,47 @@ let icons = [
     'fa-fish',
     'fa-grin'
 ]
-document.addEventListener('DOMContentLoaded', generateCards)
-
 
 let fragment = document.createDocumentFragment();
 
 
+document.addEventListener('DOMContentLoaded', generateCards)
+
 function generateCards() {
+
     console.log('starting to run function');
-        for (let i = icons.length; i > 0; i--) {
+    for (let i = icons.length; i > 0; i--) {
+            let backOfCard = document.createElement('div');
+            let frontOfCard = document.createElement('div');
+            let cardContainer = document.createElement('div');
             let randomNumber = Math.floor((Math.random() * icons.length));
-            let divToAdd = document.createElement('div');
-            divToAdd.classList.add('fas');
-            divToAdd.classList.add('card');
-            divToAdd.classList.add(`${icons[randomNumber]}`);
-            fragment.appendChild(divToAdd);
+            cardContainer.classList.add('container');
+            frontOfCard.classList.add('front');
+            backOfCard.classList.add('back', 'fas', `${icons[randomNumber]}`);
+            frontOfCard.addEventListener('click', function(event){
+                if(event.target.classList.contains('front')) {
+                     event.target.classList.remove('front');
+                     event.target.classList.add('back');
+                }
+                else {
+                     event.target.classList.remove('back');
+                     event.target.classList.add('front');
+                }
+             });
+             backOfCard.addEventListener('click', function(event){
+                 if(event.target.classList.contains('back')) {
+                     event.target.classList.remove('back');
+                     event.target.classList.add('front');
+                }
+                else {
+                     event.target.classList.remove('front');
+                     event.target.classList.add('back');
+                }
+             });
+            cardContainer.appendChild(backOfCard);
+            cardContainer.appendChild(frontOfCard);
+            fragment.appendChild(cardContainer)
             icons.splice(randomNumber, 1);
-        }
+    }
     document.body.appendChild(fragment);
 }
-
