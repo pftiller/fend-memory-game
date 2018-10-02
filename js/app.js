8897e9b38ed5f42a0bfbeec8041084adb3fe83e0
Paul Tiller <pftiller@gmail.com>
2018-10-02 17:58:25 -0500

    getting matches

--- js/app.js
+++ js/app.js
@@ -22,13 +22,15 @@ document.addEventListener('DOMContentLoaded', generateCards);
 let currentClicks = 0;
 let correctMatches = 0;
 let fragment = document.createDocumentFragment();
-
+let guessOne;
+let guessTwo;
 
 
 
 
 function generateCards() {
     console.log('starting to run function');
+    document.body.innerHTML = `<h1>Matching Game</h1>`;
     for (let i = icons.length; i > 0; i--) {
             let backOfCard = document.createElement('div');
             let frontOfCard = document.createElement('div');
@@ -36,6 +38,7 @@ function generateCards() {
             let randomNumber = Math.floor((Math.random() * icons.length));
             cardContainer.classList.add('container');
             cardContainer.addEventListener('click', function(event){
+                 
                 flipCard(event)
             });
             frontOfCard.classList.add('card', 'front');
@@ -49,51 +52,46 @@ function generateCards() {
 }
 
 
-function flipCard(event) {
+function flipCard(event, u) {
     console.log('hello', event.target);
     if(event.target.classList.contains('back')) {
-        event.target.classList.remove('back');
-        event.target.classList.add('front');
-        event.target.classList.toggle("active");;
-        subtractClick();
-    }
-    else {
-        event.target.classList.remove('front');
-        event.target.classList.toggle("active");;
-        addClick();
-    }
-}
-function addClick() {
-    currentClicks += 1;
-    if(currentClicks === 2) {
-        currentClicks = 0;
-        console.log(currentClicks);
-        compareCards();
-    }
-    else {
-        console.log(currentClicks);
-    }
-}
-
-function subtractClick() {
-    if(currentClicks > 0) {
+        event.target.classList.replace('back', 'front');
+        event.target.classList.toggle('active');  
         currentClicks -= 1;
-        console.log(currentClicks);
-    }
-}
-
-
-function compareCards() {
-    console.log('in compareCards');
-    let comparisonArray = document.querySelectorAll('.active');
-    console.log(comparisonArray);
-    if(comparisonArray[0].previousElementSibling.classList == comparisonArray[1].previousElementSibling.classList) {
-        alert('match!')
     }
     else {
-        let activeCards = document.querySelectorAll('.active');
-        activeCards.classList.toggle("active");
-        activeCards.classList.remove('front');
-        activeCards.classList.add('.back');
+        event.target.classList.replace('front','back');
+        event.target.classList.toggle('active');  
+        currentClicks += 1;
+        if(currentClicks === 1) {
+            guessOne = event.target.previousElementSibling.classList[3];
+        }
+        else if(currentClicks === 2) {
+            guessTwo = event.target.previousElementSibling.classList[3];
+            currentClicks = 0;
+            if(guessOne == guessTwo) {
+                correctMatches += 1;
+                    let firstMatchedCard = document.querySelector('.active')
+                    firstMatchedCard.classList.replace('active', 'matched');
+                    firstMatchedCard.removeEventListener('click', flipCard)
+                    let secondMatchedCard = document.querySelector('.active')
+                    secondMatchedCard.classList.replace('active', 'matched');
+                    secondMatchedCard.removeEventListener('click', flipCard)
+            }
+            else {
+                setTimeout(() => {
+                    let firstUnmatchedCard = document.querySelector('.active')
+                    firstUnmatchedCard.classList.replace('back', 'front');
+                    firstUnmatchedCard.classList.toggle('active');
+                    let secondUnmatchedCard = document.querySelector('.active')
+                    secondUnmatchedCard.classList.replace('back', 'front');
+                    secondUnmatchedCard.classList.toggle('active');
+                }, 1000);
+                
+            }
+        }
+        else {
+          
+        }
     }
 }
