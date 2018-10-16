509a24f49332182ee8154ef1f8f5639f42ff0161
Paul Tiller <pftiller@gmail.com>
2018-10-16 14:59:04 -0500

    added timer

--- js/app.js
+++ js/app.js
@@ -21,9 +21,13 @@ document.addEventListener('DOMContentLoaded', generateCards);
 
 let currentClicks = 0;
 let correctMatches = 0;
+let flips = 0;
+let seconds = 0;
 let moves = 0;
+let timer = document.createElement('div');
 let fragment = document.createDocumentFragment();
 let cardContainer = document.createElement('div');
+let timeKeeper;
 let firstTarget;
 let secondTarget;
 let guessOne;
@@ -31,7 +35,7 @@ let guessTwo;
 
 function generateCards() {
     console.log('starting to run function');
-    document.body.innerHTML = `<h1>Matching Game</h1><h2 class="left">Matches: ${correctMatches}</h2><h2 class="right">Moves: ${moves}</h2>`;
+    document.body.innerHTML = `<h1>Matching Game</h1><h2 class="left">Matches: ${correctMatches}</h2><h2 class="right">Moves: ${moves}</h2><h3>Time: ${seconds} seconds</h3>`;
     for (let i = icons.length; i > 0; i--) {
             let backOfCard = document.createElement('div');
             let frontOfCard = document.createElement('div');
@@ -56,46 +60,62 @@ function generateCards() {
 }
 
 function flipCard(event) {
+    flips +=1;
+    if(flips === 1) {
+        timeKeeper = setInterval(myTimer, 1000);
+    }
     console.log('event fired');
     var element = event.currentTarget;
-	if (element.className === "card") {
+    if (element.className === "card") {
         if(element.style.transform == "rotateY(180deg)") {
             currentClicks -= 1;  
             element.style.transform = "rotateY(0deg)";
         
     }
     else {
-      element.style.transform = "rotateY(180deg)";
-      currentClicks += 1;
-      if(currentClicks ===1) {
-            firstTarget = element;
-            guessOne = firstTarget.querySelector('div').classList[2];
-            console.log(guessOne);
-      }
-      else if(currentClicks === 2) {
-            secondTarget = element;
-            guessTwo = secondTarget.querySelector('div').classList[2];
-            document.querySelector('.right').innerText=`Moves: ${moves +=1}`;
-            currentClicks = 0;
-                if(guessOne == guessTwo) {
-                    document.querySelector('.left').innerText=`Matches: ${correctMatches +=1}`;
-                    firstTarget.removeEventListener('click', flipCard);
-                    firstTarget.querySelector('div').classList.add('matched');
-                    secondTarget.removeEventListener('click', flipCard);
-                    secondTarget.querySelector('div').classList.add('matched');
-                    if(correctMatches===8) {
+    element.style.transform = "rotateY(180deg)";
+    currentClicks += 1;
+        if(currentClicks ===1) {
+                firstTarget = element;
+                guessOne = firstTarget.querySelector('div').classList[2];
+                console.log(guessOne);
+        }
+        else if(currentClicks === 2) {
+                secondTarget = element;
+                guessTwo = secondTarget.querySelector('div').classList[2];
+                document.querySelector('.right').innerText=`Moves: ${moves +=1}`;
+                currentClicks = 0;
+                    if(guessOne == guessTwo) {
+                        document.querySelector('.left').innerText=`Matches: ${correctMatches +=1}`;
+                        firstTarget.removeEventListener('click', flipCard);
+                        firstTarget.querySelector('div').classList.add('matched');
+                        secondTarget.removeEventListener('click', flipCard);
+                        secondTarget.querySelector('div').classList.add('matched');
+                        if(correctMatches===8) {
+                            clearInterval(timeKeeper);
+                            setTimeout(() => {
+                                document.querySelector('.modal').classList.toggle('show-modal');
+                            }, 2000);
+                        }
+                    }
+                    else {
                         setTimeout(() => {
-                            document.querySelector('.modal').classList.toggle('show-modal');
+                            firstTarget.style.transform = "rotateY(0deg)";
+                            secondTarget.style.transform = "rotateY(0deg)";
                         }, 2000);
                     }
-                }
-                else {
-                    setTimeout(() => {
-                        firstTarget.style.transform = "rotateY(0deg)";
-                        secondTarget.style.transform = "rotateY(0deg)";
-                    }, 2000);
                 }
             }
         }
     }
+
+
+
+function myTimer() {
+    seconds +=1;
+    document.querySelector('h3').innerText=`Time: ${seconds} seconds`
+    console.log('time: ', seconds);
 };
+
+
+    
